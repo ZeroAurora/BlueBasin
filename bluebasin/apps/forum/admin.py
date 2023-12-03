520f999bd8dcb1fdb18901f3ede893383f305549
ZeroAurora <zeroaurora233@outlook.com>
2023-12-03 16:21:05 +0800

    Fix Post searching

--- bluebasin/apps/forum/admin.py
+++ bluebasin/apps/forum/admin.py
@@ -5,9 +5,11 @@ from .models import *
 # admin.site.register(Tag)
 # admin.site.register(AvaliableReaction)
 
+
 @admin.register(Post)
 class PostAdmin(admin.ModelAdmin):
     list_display = ["pk", "title", "author", "created_at"]
-    search_fields = ["pk", "title", "author", "content"]
+    search_fields = ["pk", "title", "author__name", "author__user__username", "content"]
+
 
 # admin.site.register(Reaction)
